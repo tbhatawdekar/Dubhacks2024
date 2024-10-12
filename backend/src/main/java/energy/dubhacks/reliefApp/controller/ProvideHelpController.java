a20ef625ea59bc795565eba4e5111bfce2b0a1ad
Isayiah Lim <isayiahu@outlook.com>
2024-10-12 16:51:26 -0700

    merged with both backend features

--- backend/src/main/java/energy/dubhacks/reliefApp/controller/ProvideHelpController.java
+++ backend/src/main/java/energy/dubhacks/reliefApp/controller/ProvideHelpController.java
@@ -1,52 +1,23 @@
-//package energy.dubhacks.reliefApp.controller;
-//
-//import energy.dubhacks.reliefApp.dto.UserDTO;
-//import energy.dubhacks.reliefApp.model.User;
-//import energy.dubhacks.reliefApp.service.UserService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//import java.util.List;
-//
-//@RestController
-//@RequestMapping("/provide")
-//public class ProvideHelpController {
-//
-//    @Autowired
-//    private UserService userService;
-//
-//    // Get all users
-//    @GetMapping
-//    public ResponseEntity<List<User>> getAllUsers() {
-//        List<User> users = userService.getAllUsers();
-//        return ResponseEntity.ok(users);
-//    }
-//
-//    // Get a user by ID
-//    @GetMapping("/{id}")
-//    public ResponseEntity<User> getUserById(@PathVariable Long id) {
-//        User user = userService.getUserById(id);
-//        return ResponseEntity.ok(user);
-//    }
-//
-//    // Create a new user
-//    @PostMapping
-//    public ResponseEntity<User> createUser(@RequestBody UserDTO userDTO) {
-//        User createdUser = userService.createUser(userDTO);
-//        return ResponseEntity.ok(createdUser);
-//    }
-//
-//    // Update an existing user
-//    @PutMapping("/{id}")
-//    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody UserDTO userDTO) {
-//        User updatedUser = userService.updateUser(id, userDTO);
-//        return ResponseEntity.ok(updatedUser);
-//    }
-//
-//    // Delete a user
-//    @DeleteMapping("/{id}")
-//    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
-//        userService.deleteUser(id);
-//        return ResponseEntity.noContent().build();
-//    }
-//}
+package energy.dubhacks.reliefApp.controller;
+
+import energy.dubhacks.reliefApp.model.Post;
+import energy.dubhacks.reliefApp.service.ProvideService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/provide")
+public class ProvideHelpController {
+
+    @Autowired
+    private ProvideService provideService;
+
+    // Gets all posts
+    @GetMapping
+    public ResponseEntity<List<Post>> getAllPosts() {
+        List<Post> users = provideService.getAllPosts();
+        return ResponseEntity.ok(users);
+    }
+}
--- backend/src/main/java/energy/dubhacks/reliefApp/controller/SeekHelpController.java
+++ backend/src/main/java/energy/dubhacks/reliefApp/controller/SeekHelpController.java
@@ -1,37 +1,28 @@
-//package energy.dubhacks.reliefApp.controller;
-//
-//import energy.dubhacks.reliefApp.dto.SeekPostDTO;
-//import energy.dubhacks.reliefApp.model.Resource;
-//import energy.dubhacks.reliefApp.service.ResourceService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//
-//@RestController
-//@RequestMapping("/seek")
-//public class SeekHelpController {
-//
-//    @Autowired
-//    private ResourceService resourceService;
-//
-//    // Create a new post
-//    @PostMapping
-//    public ResponseEntity<Resource> createResource(@RequestBody SeekPostDTO seekPostDTO) {
-//        Resource createdResource = resourceService.createResource(seekPostDTO);
-//        return ResponseEntity.ok(createdResource);
-//    }
-//
-//    // // Update an existing post
-//    // @PutMapping("/{id}")
-//    // public ResponseEntity<Resource> updateResource(@PathVariable Long id, @RequestBody ResourceDTO resourceDTO) {
-//    //     Resource updatedResource = resourceService.updateResource(id, resourceDTO);
-//    //     return ResponseEntity.ok(updatedResource);
-//    // }
-//
-//    // // Delete a post
-//    // @DeleteMapping("/{id}")
-//    // public ResponseEntity<Void> deleteResource(@PathVariable Long id) {
-//    //     resourceService.deleteResource(id);
-//    //     return ResponseEntity.noContent().build();
-//    // }
-//}
+package energy.dubhacks.reliefApp.controller;
+
+import energy.dubhacks.reliefApp.dto.SeekPostDTO;
+import energy.dubhacks.reliefApp.model.Post;
+import energy.dubhacks.reliefApp.service.SeekService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/seek")
+public class SeekHelpController {
+
+    @Autowired
+    private SeekService seekService;
+
+    // Create a new post
+    @PostMapping
+    public ResponseEntity<Post> createResource(@RequestBody SeekPostDTO seekPostDTO) {
+        Post createdResource = seekService.createResource(seekPostDTO);
+        return ResponseEntity.ok(createdResource);
+    }
+
+    // Update an existing post?
+
+    // Delete a post?
+
+}
