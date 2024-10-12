80243fc052d08355eec3d83f629755ad30c4cab3
lawrencewang <lawrencewang2004@gmail.com>
2024-10-12 15:30:33 -0700

    Config and basic resource setup

--- backend/src/main/java/energy/dubhacks/reliefApp/service/ResourceService.java
+++ backend/src/main/java/energy/dubhacks/reliefApp/service/ResourceService.java
@@ -5,13 +5,21 @@ import energy.dubhacks.reliefApp.model.Resource;
 import energy.dubhacks.reliefApp.repository.ResourceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
 import java.util.List;
 
 @Service
 public class ResourceService {
 
-    @Autowired
     private ResourceRepository resourceRepository;
+    private WebClient webClient;
+
+    @Autowired
+    public ResourceService(ResourceRepository resourceRepository, WebClient.Builder webClientBuilder) {
+        this.resourceRepository = new ResourceRepository();
+        this.webClient = webClientBuilder.build();
+    }
 
     // Retrieve all resources
     public List<Resource> getAllResources() {
