cb881deb66872adb2364ffb51877e6b96e6695c0
Isayiah Lim <isayiahu@outlook.com>
2024-10-12 14:20:03 -0700

    git workflow demo

--- backend/src/test/java/energy/dubhacks/reliefApp/ReliefAppApplicationTests.java
+++ backend/src/test/java/energy/dubhacks/reliefApp/ReliefAppApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package energy.dubhacks.reliefApp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class ReliefAppApplicationTests {
 
 	@Test
 	void contextLoads() {
